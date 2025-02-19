b1398e1d7a1d5e467e009572b51582af4cffed0f
ShaZimbo <shanwratten@gmail.com>
2025-02-19 16:15:03 +0000

    Update gamword.py

--- Projects/Game of Words/gamword.py
+++ Projects/Game of Words/gamword.py
@@ -29,8 +29,8 @@ def menu_selection():
 correct = []
 misplaced = []
 incorrect = []
-max_turn = 5
-current_turn = 0
+MAX_TURN = 5
+CURRENT_TURN = 0
 
 user_name = input("Enter your name:\n")
 print(f"\nHello {user_name}! Welcome to the game of words.\n"
@@ -42,19 +42,19 @@ while True:
     menu_choice = menu_selection()
 
     while menu_choice == "p":
-        current_turn = 0
+        CURRENT_TURN = 0
         rand_word = random.choice(get_words())
-        while current_turn < max_turn:
+        while CURRENT_TURN < MAX_TURN:
             user_guess = input("Enter your guess:\n").lower()
             if len(user_guess) != 5:
                 print("\033[31m\n"
                       "Your guess must be 5 characters long.\n\033[0m"
                       )
-                user_guess = input("Enter your guess:\n").lower()
-            current_turn += 1
+                continue
+            CURRENT_TURN += 1
             if user_guess == rand_word:
                 print(f"\033[32m\nCongratulations!\033[0m\n\n"
-                      f"You have guessed the word in {current_turn} turns."
+                      f"You have guessed the word in {CURRENT_TURN} turns."
                       )
                 break
             else:
@@ -64,10 +64,9 @@ while True:
                 for i in range(5):
                     if user_guess[i] == rand_word[i]:
                         correct.append(user_guess[i])
-                    if (user_guess[i] in rand_word and
-                            user_guess[i] not in correct):
+                    elif user_guess[i] in rand_word:
                         misplaced.append(user_guess[i])
-                    if user_guess[i] not in rand_word:
+                    else:
                         incorrect.append(user_guess[i])
                 print(f"Correct letters in correct position:\n"
                       f"\033[32m{correct}\033[0m\n"
@@ -75,9 +74,9 @@ while True:
                       f"\033[33m{misplaced}\033[0m\n"
                       f"Incorrect letters:\n"
                       f"\033[31m{incorrect}\033[0m\n"
-                      f"Turns left: {max_turn - current_turn}"
+                      f"Turns left: {MAX_TURN - CURRENT_TURN}\n"
                       )
-            if current_turn == max_turn:
+            if CURRENT_TURN == MAX_TURN:
                 print(f"\033[31mSorry!\033[0m\n"
                       f"You have exhausted all your turns.\n"
                       f"The word was {rand_word}"
