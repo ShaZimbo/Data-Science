df731d8f381f83f9133d35d89f2fad4d00b2d9bd
ShaZimbo <shanwratten@gmail.com>
2025-05-08 13:04:18 +0100

    Add files via upload

---
+++ Python Basics/Turtle GUI/random_walk.py
@@ -0,0 +1,28 @@
+""" Take the turtle on a random walk """
+import random
+from turtle import Turtle, Screen, colormode
+
+colormode(255)
+tommy = Turtle()
+
+
+def ranc():
+    """ Function to choose color """
+    r = random.randint(0, 255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+    return tommy.color(r, b, g)
+
+
+angle = [0, 90, 180, 270]
+tommy.speed(0)
+tommy.shape("turtle")
+tommy.pensize(10)
+
+for _ in range(150):
+    ranc()
+    tommy.setheading(random.choice(angle))
+    tommy.fd(20)
+
+SCREEN = Screen()
+SCREEN.exitonclick()
