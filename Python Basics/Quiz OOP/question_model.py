cb57fd38344014866414d3703aabdb1a6d4eab51
ShaZimbo <shanwratten@gmail.com>
2025-05-09 08:42:49 +0100

    Delete Python Basics/Quiz OOP directory

--- Python Basics/Quiz OOP/question_model.py
+++
@@ -1,8 +0,0 @@
-""" Class to select questions and answers """
-
-
-class Question:
-    """ Class to select questions and answers"""
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
