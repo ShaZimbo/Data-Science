b12c72c1a0021e3accf7e20f87830b75405996a0
ShaZimbo <shanwratten@gmail.com>
2025-05-20 15:07:23 +0100

    Add files via upload

---
+++ Python Intermediate/Mail Merge Project Start/main.py
@@ -0,0 +1,20 @@
+""" Create invitations using the template and names """
+LETTER_PATH = (
+    ".\\Mail Merge Project Start\\Input\\Letters\\starting_letter.txt"
+)
+NAMES_PATH = (
+    ".\\Mail Merge Project Start\\Input\\Names\\invited_names.txt"
+)
+
+with open(NAMES_PATH, "r", encoding="utf-8") as names:
+    for line in names.readlines():
+        name = line.strip("\n")
+        READY_PATH = (
+                    f".\\Mail Merge Project Start\\Output\\ReadyToSend\\"
+                    f"{name}.txt"
+        )
+        with open(LETTER_PATH, "r", encoding="utf-8") as letter:
+            with open(READY_PATH, "w", encoding="utf-8") as invite:
+                for lines in letter.readlines():
+                    x = lines.replace("[name]", f"{name}")
+                    invite.write(x)
